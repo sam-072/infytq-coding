33b3c5026658cb1e45a0f51117749fa2d43db2b3
sam-072 <71545419+sam-072@users.noreply.github.com>
2022-02-08 11:25:22 +0530

    Update coding.py

--- coding.py
+++ coding.py
@@ -410,5 +410,55 @@ for i in range(n):
 
 # output: 134,127,126,114,179,145,164,160,81,120,134,119,125,164,87,119,114,144,236,120
 
+# >>>>>>>>>>> yash 2nd question largest palidromic number
+
+from collections import defaultdict
+
+def possibility(m, length, s):
+    odd = 0
+    for i in range(0, length):
+ 
+        if m[int(s[i])] & 1:
+            odd += 1
+        if odd > 1:
+            return False
+     
+    return True
+def largestPalindrome(s):
+    l = len(s)
+    m = defaultdict(lambda:0)
+    for i in range(0, l):
+        m[int(s[i])] += 1
+    if possibility(m, l, s) == False:
+        print(-1)
+        return
+    largest = [None] * l
+    front = 0
+    for i in range(9, -1, -1):
+        if m[i] & 1:
+            largest[l // 2] = chr(i + 48)
+            m[i] -= 1
+            while m[i] > 0:
+                largest[front] = chr(i + 48)
+                largest[l - front - 1] = chr(i + 48)
+                m[i] -= 2
+                front += 1
+             
+        else:
+            while m[i] > 0:
+                largest[front] = chr(i + 48)
+                largest[l - front - 1] = chr(i + 48)
+                m[i] -= 2
+                front += 1
+    for i in range(0, l):
+        print(largest[i], end = "")
+if __name__ == "__main__":
+    s = input()
+    largestPalindrome(s)
+    
+# input : 2341    output : -1
+# input : 388003     output : 830038
+
+
  
     
